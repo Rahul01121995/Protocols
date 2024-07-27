06f2dcb21d794d199b21ad936451ebf5edf769a0
Rahul01121995 <heerekarrahul@gmail.com>
2024-07-27 19:15:37 +0530

    function with Arguments

--- Protocols/Protocols/ViewController.swift
+++ Protocols/Protocols/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addTwoIntegers(firstDigit: 12, secondDigit: 34)
+        subtractionTwoIntegers(firstDigit: 24, secondDigit: 46)
     }
-
-
+    
+    func addTwoIntegers(firstDigit: Int, secondDigit: Int) {
+        var answer = firstDigit + secondDigit
+        print(answer)
+    }
+    
+    func addTwoIntegers() {
+        var firstDigit = 12
+        var secondDigit = 34
+        var answer = firstDigit + secondDigit
+        print(answer)
+    }
+    
+    func subtractionTwoIntegers(firstDigit: Int, secondDigit: Int){
+        var answer = firstDigit - secondDigit
+        print(answer)
+    }
+    
 }
 
